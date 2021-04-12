8b0168d62b0b7bdfd2b0eaffa5b570eeac0ac1a7
Alex Belov <belov.alexandr1991@gmail.com>
2021-04-12 10:18:32 +0800

    Initial Commit

---
+++ Test Assigment UI AutomationTests/Test_Assigment_UI_AutomationTests.swift
@@ -0,0 +1,33 @@
+//
+//  Test_Assigment_UI_AutomationTests.swift
+//  Test Assigment UI AutomationTests
+//
+//  Created by Alexios on 12/4/21.
+//
+
+import XCTest
+@testable import Test_Assigment_UI_Automation
+
+class Test_Assigment_UI_AutomationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
