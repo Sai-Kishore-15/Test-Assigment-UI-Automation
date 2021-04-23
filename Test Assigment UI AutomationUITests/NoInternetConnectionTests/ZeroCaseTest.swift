e5955baa5c967e77d9b89da2b2843ad32fe7bd2b
Sai Kishore <saikishore1997@gmail.com>
2021-04-23 16:35:29 +0530

    Test - No Internet Connectivity / Zero Case

---
+++ Test Assigment UI AutomationUITests/NoInternetConnectionTests/ZeroCaseTest.swift
@@ -0,0 +1,36 @@
+//
+//  ZeroCaseTest.swift
+//  Test Assigment UI AutomationUITests
+//
+//  Created by Sai Kishore on 23/04/21.
+//
+
+import XCTest
+
+class ZeroCaseTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func test_isZeroCaseVisibleOnLaunch() {
+        // Succeeds when Internet Connectivity is Off
+        // Fails when Internet Connectivity is On
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        let zeroCase = app.otherElements["zero-case-view"]
+        let tryAgainbutton = app.otherElements["zero-case-view"].buttons["try-again"]
+        let noConnectionLabel = app.otherElements["zero-case-view"].staticTexts["text-no-connection"]
+        
+        XCTAssertTrue(zeroCase.exists, "Zero Case does not exist")
+        XCTAssertTrue(tryAgainbutton.exists)
+        XCTAssertTrue(noConnectionLabel.exists)
+    }
+}
